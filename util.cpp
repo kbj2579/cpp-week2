1eb94b3141bcbbe6b66c38cdbd515bd8e810776c
kbj2579 <qjawns2579@naver.com>
2024-03-17 15:27:04 +0900

    fix: 정답의 숫자가 중복되지 않도록 변경

--- util.cpp
+++ util.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
-#include<string>
+#include <string>
 #include <stdlib.h>
 #include <time.h>
 std::string util(){
     std::string s = "";
     int a, b, c;
-    a = rand() % 10;
-    b = rand() % 10;
-    c = rand() % 10;
+    while(1){
+        a = rand() % 10;
+        b = rand() % 10;
+        c = rand() % 10;
+        if (a != b && b != c && c != a) break;
+    }
+    
     s += std::to_string(a);
     s += std::to_string(b);
     s += std::to_string(c);
